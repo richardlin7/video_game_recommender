5c38f3efdfe5d1d8577205623c0d439ad5fd65fa
Richard Lin <richard9774lin@gmail.com>
2025-02-19 19:54:59 -0500

    Updated Chakra UI
    Updated UI to appear properly

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
-  },
+	baseURL: "https://api.rawg.io/api",
+	params: {
+		key: "dacabcce9ceb45c5bf084c812e636a56",
+	},
 });
